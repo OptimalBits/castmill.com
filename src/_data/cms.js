194cb1e40b6886a1fa7872c885b5ae61764c9ccd
Manuel Astudillo <manuel@optimalbits.com>
2022-04-12 14:52:16 +0800

    fix: fix some html errors

--- src/_data/cms.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-module.exports = async () => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-
-  return data.slice(0, 5);
-};
